522aa1de562d27eb5b1d02b3db6febe2202da408
Dee121-ala <deekshitaalavala@gmail.com>
2020-10-06 15:20:52 +0530

    [deekshita] Add : Welcome message

---
+++ UserValidation/src/main/java/com/capg/uservalidation/App.java
@@ -0,0 +1,21 @@
+package com.capg.uservalidation;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private static final Logger LOG = LogManager.getLogger(App.class);
+	
+	
+    public static void main( String[] args )
+    {
+    	
+		
+        LOG.info("Welcome to user validation ");
+        
+    }
+}
